4fe1a81ce7e16413b01467a2f88058127941170e
Zahary Karadjov <zahary@gmail.com>
2020-07-21 22:17:11 +0300

    Add helpers for using Web3 types on the Confutils command-line

---
+++ web3/confutils_defs.nim
@@ -0,0 +1,17 @@
+import
+  ethtypes
+
+func parseCmdArg*(T: type Address, input: TaintedString): T
+                 {.raises: [ValueError, Defect].} =
+  fromHex(T, string input)
+
+func completeCmdArg*(T: type Address, input: TaintedString): seq[string] =
+  return @[]
+
+func parseCmdArg*(T: type BlockHash, input: TaintedString): T
+                 {.raises: [ValueError, Defect].} =
+  fromHex(T, string input)
+
+func completeCmdArg*(T: type BlockHash, input: TaintedString): seq[string] =
+  return @[]
+
