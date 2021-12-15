47acb280fd998a1dfd20cfe066bb6b0289fd6266
Mark Spanbroek <mark@spanbroek.net>
2021-12-15 17:34:58 +0100

    Move generated types from `encoding` to `ethtypes`

--- web3/ethtypes.nim
+++ web3/ethtypes.nim
@@ -1,6 +1,8 @@
 import
   options, json, hashes, typetraits,
   stint, stew/byteutils
+import std/macros
+import std/math
 
 type
   SyncObject* = object
@@ -290,3 +292,89 @@ template len*(data: DynamicBytes): int =
 
 func `$`*[minLen, maxLen](data: DynamicBytes[minLen, maxLen]): string =
   "0x" & byteutils.toHex(distinctbase(data))
+
+macro makeTypeEnum(): untyped =
+  ## This macro creates all the various types of Solidity contracts and maps
+  ## them to the type used for their encoding. It also creates an enum to
+  ## identify these types in the contract signatures, along with encoder
+  ## functions used in the generated procedures.
+  result = newStmtList()
+  var lastpow2: int
+  for i in countdown(256, 8, 8):
+    let
+      identUint = newIdentNode("Uint" & $i)
+      identInt = newIdentNode("Int" & $i)
+    if ceil(log2(i.float)) == floor(log2(i.float)):
+      lastpow2 = i
+    if i notin {256, 125}: # Int/Uint256/128 are already defined in stint. No need to repeat.
+      result.add quote do:
+        type
+          `identUint`* = Stuint[`lastpow2`]
+          `identInt`* = Stint[`lastpow2`]
+  let
+    identUint = ident("Uint")
+    identInt = ident("Int")
+    identBool = ident("Bool")
+  result.add quote do:
+    type
+      `identUint`* = Uint256
+      `identInt`* = Int256
+      `identBool`* = distinct Int256
+
+  for m in countup(8, 256, 8):
+    let
+      identInt = ident("Int" & $m)
+      identUint = ident("Uint" & $m)
+      identFixed = ident "Fixed" & $m
+      identUfixed = ident "Ufixed" & $m
+      identT = ident "T"
+    result.add quote do:
+      # Fixed stuff is not actually implemented yet, these procedures don't
+      # do what they are supposed to.
+      type
+        `identFixed`*[N: static[int]] = distinct `identInt`
+        `identUfixed`*[N: static[int]] = distinct `identUint`
+
+      # func to*(x: `identInt`, `identT`: typedesc[`identFixed`]): `identT` =
+      #   T(x)
+
+      # func to*(x: `identUint`, `identT`: typedesc[`identUfixed`]): `identT` =
+      #   T(x)
+
+      # func encode*[N: static[int]](x: `identFixed`[N]): EncodeResult =
+      #   encode(`identInt`(x) * (10 ^ N).to(`identInt`))
+
+      # func encode*[N: static[int]](x: `identUfixed`[N]): EncodeResult =
+      #   encode(`identUint`(x) * (10 ^ N).to(`identUint`))
+
+      # func decode*[N: static[int]](input: string, to: `identFixed`[N]): `identFixed`[N] =
+      #   decode(input, `identInt`) div / (10 ^ N).to(`identInt`)
+
+      # func decode*[N: static[int]](input: string, to: `identUfixed`[N]): `identFixed`[N] =
+      #   decode(input, `identUint`) div / (10 ^ N).to(`identUint`)
+
+  let
+    identFixed = ident("Fixed")
+    identUfixed = ident("Ufixed")
+  result.add quote do:
+    type
+      `identFixed`* = distinct Int128
+      `identUfixed`* = distinct Uint128
+  for i in 1..256:
+    let
+      identBytes = ident("Bytes" & $i)
+      identResult = ident "result"
+    result.add quote do:
+      type
+        `identBytes`* = FixedBytes[`i`]
+
+  #result.add newEnum(ident "FieldKind", fields, public = true, pure = true)
+
+makeTypeEnum()
+
+proc parse*(T: type Bool, val: bool): T =
+  let i = if val: 1 else: 0
+  T i.i256
+
+proc `==`*(a: Bool, b: Bool): bool =
+  Int256(a) == Int256(b)
