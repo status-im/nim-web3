7daafe7b9cdfc3743c4b924eca6621dc928cf138
Zahary Karadjov <zahary@gmail.com>
2022-01-24 15:06:12 +0200

    More complete Engine API v1.0.0-alpha.5
    * Added error codes
    * Renamed ExecutionPayloadResponse.message to validationError

--- web3/engine_api_types.nim
+++ web3/engine_api_types.nim
@@ -22,7 +22,7 @@ type
   ExecutePayloadResponse* = object
     status*: PayloadExecutionStatus
     latestValidHash*: Option[BlockHash]
-    message*: Option[string]
+    validationError*: Option[string]
 
   # https://github.com/ethereum/execution-apis/blob/v1.0.0-alpha.5/src/engine/specification.md#forkchoicestatev1
   ForkchoiceStateV1* = object
@@ -37,3 +37,13 @@ type
   ForkchoiceUpdatedResponse* = object
     status*: ForkchoiceUpdatedStatus
     payloadId*: Option[PayloadID]
+
+const
+  engineApiParseError* = - 32700
+  engineApiInvalidRequest* = -32600
+  engineApiMethodNotFound* = -32601
+  engineApiInvalidParams* = -32602
+  engineApiInternalError* = -32603
+  engineApiServerError* = -32000
+  engineApiUnknownPayload* = -32001
+  engineApiInvalidTerminalBlock* = -32002
