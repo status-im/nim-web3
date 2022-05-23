755b6dc92b1545d6c9eb242c551e8188e35ffe5d
Dustin Brody <tersec@users.noreply.github.com>
2022-05-23 14:42:56 +0000

    alpha.9 engine API

--- web3/engine_api_types.nim
+++ web3/engine_api_types.nim
@@ -7,51 +7,52 @@ export
   ethtypes
 
 type
-  # https://github.com/ethereum/execution-apis/blob/v1.0.0-alpha.8/src/engine/specification.md#payloadattributesv1
+  # https://github.com/ethereum/execution-apis/blob/v1.0.0-alpha.9/src/engine/specification.md#payloadattributesv1
   PayloadAttributesV1* = object
     timestamp*: Quantity
     prevRandao*: FixedBytes[32]
     suggestedFeeRecipient*: Address
 
-  # https://github.com/ethereum/execution-apis/blob/v1.0.0-alpha.8/src/engine/specification.md#payloadstatusv1
+  # https://github.com/ethereum/execution-apis/blob/v1.0.0-alpha.9/src/engine/specification.md#payloadstatusv1
   PayloadExecutionStatus* {.pure.} = enum
     valid                  = "VALID"
     invalid                = "INVALID"
     syncing                = "SYNCING"
     accepted               = "ACCEPTED"
     invalid_block_hash     = "INVALID_BLOCK_HASH"
-    invalid_terminal_block = "INVALID_TERMINAL_BLOCK"
 
   PayloadStatusV1* = object
     status*: PayloadExecutionStatus
     latestValidHash*: Option[BlockHash]
     validationError*: Option[string]
 
-  # https://github.com/ethereum/execution-apis/blob/v1.0.0-alpha.8/src/engine/specification.md#forkchoicestatev1
+  # https://github.com/ethereum/execution-apis/blob/v1.0.0-alpha.9/src/engine/specification.md#forkchoicestatev1
   ForkchoiceStateV1* = object
     headBlockHash*: BlockHash
     safeBlockHash*: BlockHash
     finalizedBlockHash*: BlockHash
 
-  # https://github.com/ethereum/execution-apis/blob/v1.0.0-alpha.8/src/engine/specification.md#response-1
+  # https://github.com/ethereum/execution-apis/blob/v1.0.0-alpha.9/src/engine/specification.md#response-1
   PayloadID* = FixedBytes[8]
 
   ForkchoiceUpdatedResponse* = object
     payloadStatus*: PayloadStatusV1
     payloadId*: Option[PayloadID]
 
-  # https://github.com/ethereum/execution-apis/blob/v1.0.0-alpha.8/src/engine/specification.md#transitionconfigurationv1
+  # https://github.com/ethereum/execution-apis/blob/v1.0.0-alpha.9/src/engine/specification.md#transitionconfigurationv1
   TransitionConfigurationV1* = object
     terminalTotalDifficulty*: UInt256
     terminalBlockHash*: BlockHash
     terminalBlockNumber*: Quantity
 
 const
-  # https://github.com/ethereum/execution-apis/blob/v1.0.0-alpha.8/src/engine/specification.md#errors
+  # https://github.com/ethereum/execution-apis/blob/v1.0.0-alpha.9/src/engine/specification.md#errors
   engineApiParseError* = - 32700
   engineApiInvalidRequest* = -32600
   engineApiMethodNotFound* = -32601
   engineApiInvalidParams* = -32602
   engineApiInternalError* = -32603
   engineApiServerError* = -32000
-  engineApiUnknownPayload* = -32001
+  engineApiUnknownPayload* = -38001
+  engineApiInvalidForkchoiceState* = -38002
+  engineApiInvalidPayloadAttributes* = -38003
