f1bb95ab8125dc84f701adc37eaa18eb339a92fe
Dustin Brody <tersec@users.noreply.github.com>
2022-07-06 23:58:54 +0000

    enforce JSON-RPC Quantity spec syntax rules

--- web3/conversions.nim
+++ web3/conversions.nim
@@ -1,3 +1,9 @@
+# Copyright (c) 2019-2022 Status Research & Development GmbH
+# Licensed and distributed under either of
+#   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
+#   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
+# at your option. This file may not be copied, modified, or distributed except according to those terms.
+
 import
   std/[json, options, strutils, strformat, tables, typetraits],
   stint, stew/byteutils, json_serialization, faststreams/textio,
@@ -6,47 +12,65 @@ import
 
 from json_rpc/rpcserver import expect
 
-proc `%`*(n: Int256|UInt256): JsonNode = %("0x" & n.toHex)
+template invalidQuantityPrefix(s: string): bool =
+  # https://ethereum.org/en/developers/docs/apis/json-rpc/#hex-value-encoding
+  # https://github.com/ethereum/execution-apis/blob/v1.0.0-alpha.9/src/engine/specification.md#structures
+  # "When encoding quantities (integers, numbers): encode as hex, prefix with
+  # "0x", the most compact representation (slight exception: zero should be
+  # represented as "0x0")."
+  #
+  # strutils.parseHexStr treats 0x as optional otherwise. UInt256.parse treats
+  # standalone "0x" as valid input.
+  (not s.startsWith "0x") or s == "0x" or (s != "0x0" and s.startsWith "0x0")
+
+func `%`*(n: Int256|UInt256): JsonNode = %("0x" & n.toHex)
 
 # allows UInt256 to be passed as a json string
-proc fromJson*(n: JsonNode, argName: string, result: var UInt256) =
+func fromJson*(n: JsonNode, argName: string, result: var UInt256) =
   # expects base 16 string, starting with "0x"
   n.kind.expect(JString, argName)
   let hexStr = n.getStr()
   if hexStr.len > 64 + 2: # including "0x"
     raise newException(ValueError, "Parameter \"" & argName & "\" value too long for UInt256: " & $hexStr.len)
+  if hexStr.invalidQuantityPrefix:
+    raise newException(ValueError, "Parameter \"" & argName & "\" value has invalid leading 0")
   result = hexStr.parse(StUint[256], 16) # TODO: Handle errors
 
 # allows ref UInt256 to be passed as a json string
-proc fromJson*(n: JsonNode, argName: string, result: var ref UInt256) =
+func fromJson*(n: JsonNode, argName: string, result: var ref UInt256) =
   # expects base 16 string, starting with "0x"
   n.kind.expect(JString, argName)
   let hexStr = n.getStr()
   if hexStr.len > 64 + 2: # including "0x"
     raise newException(ValueError, "Parameter \"" & argName & "\" value too long for UInt256: " & $hexStr.len)
+  if hexStr.invalidQuantityPrefix:
+    raise newException(ValueError, "Parameter \"" & argName & "\" value has invalid leading 0")
   new result
   result[] = hexStr.parse(StUint[256], 16) # TODO: Handle errors
 
-proc bytesFromJson(n: JsonNode, argName: string, result: var openArray[byte]) =
+func bytesFromJson(n: JsonNode, argName: string, result: var openArray[byte]) =
   n.kind.expect(JString, argName)
   let hexStr = n.getStr()
   if hexStr.len != result.len * 2 + 2: # including "0x"
     raise newException(ValueError, "Parameter \"" & argName & "\" value wrong length: " & $hexStr.len)
   hexToByteArray(hexStr, result)
 
-proc fromJson*[N](n: JsonNode, argName: string, result: var FixedBytes[N]) {.inline.} =
+func fromJson*[N](n: JsonNode, argName: string, result: var FixedBytes[N])
+    {.inline.} =
   # expects base 16 string, starting with "0x"
   bytesFromJson(n, argName, array[N, byte](result))
 
-proc fromJson*(n: JsonNode, argName: string, result: var DynamicBytes) {.inline.} =
+func fromJson*(n: JsonNode, argName: string, result: var DynamicBytes)
+    {.inline.} =
   n.kind.expect(JString, argName)
   result = fromHex(type result, n.getStr())
 
-proc fromJson*(n: JsonNode, argName: string, result: var Address) {.inline.} =
+func fromJson*(n: JsonNode, argName: string, result: var Address) {.inline.} =
   # expects base 16 string, starting with "0x"
   bytesFromJson(n, argName, array[20, byte](result))
 
-proc fromJson*(n: JsonNode, argName: string, result: var TypedTransaction) {.inline.} =
+func fromJson*(n: JsonNode, argName: string, result: var TypedTransaction)
+    {.inline.} =
   let hexStrLen = n.getStr().len
   if hexStrLen < 2:
     # "0x" prefix
@@ -58,14 +82,15 @@ proc fromJson*(n: JsonNode, argName: string, result: var TypedTransaction) {.inl
   distinctBase(result).setLen((hexStrLen - 2) div 2)
   bytesFromJson(n, argName, distinctBase(result))
 
-proc fromJson*(n: JsonNode, argName: string, result: var Quantity) {.inline.} =
-  if n.kind == JInt:
-    result = Quantity(n.getBiggestInt)
-  else:
-    n.kind.expect(JString, argName)
-    result = Quantity(parseHexInt(n.getStr))
+func fromJson*(n: JsonNode, argName: string, result: var Quantity) {.inline.} =
+  n.kind.expect(JString, argName)
+  let hexStr = n.getStr
+  if hexStr.invalidQuantityPrefix:
+    raise newException(ValueError, "Parameter \"" & argName & "\" value has invalid leading 0")
+  result = Quantity(parseHexInt(hexStr))
 
-func getEnumStringTable(enumType: typedesc): Table[string, enumType] {.compileTime.} =
+func getEnumStringTable(enumType: typedesc): Table[string, enumType]
+    {.compileTime.} =
   var res: Table[string, enumType]
   # Not intended for enums with ordinal holes or repeated stringification
   # strings.
@@ -73,8 +98,9 @@ func getEnumStringTable(enumType: typedesc): Table[string, enumType] {.compileTi
     res[$value] = value
   res
 
-proc fromJson*(
-    n: JsonNode, argName: string, result: var PayloadExecutionStatus) {.inline.} =
+func fromJson*(
+    n: JsonNode, argName: string, result: var PayloadExecutionStatus)
+    {.inline.} =
   n.kind.expect(JString, argName)
   const enumStrings = static: getEnumStringTable(type(result))
   try:
@@ -83,20 +109,20 @@ proc fromJson*(
     raise newException(
       ValueError, "Parameter \"" & argName & "\" value invalid: " & n.getStr)
 
-proc `%`*(v: Quantity): JsonNode =
-  result = %encodeQuantity(v.uint64)
+func `%`*(v: Quantity): JsonNode =
+  %encodeQuantity(v.uint64)
 
-proc `%`*[N](v: FixedBytes[N]): JsonNode =
-  result = %("0x" & array[N, byte](v).toHex)
+func `%`*[N](v: FixedBytes[N]): JsonNode =
+  %("0x" & array[N, byte](v).toHex)
 
-proc `%`*(v: DynamicBytes): JsonNode =
-  result = %("0x" & toHex(v))
+func `%`*(v: DynamicBytes): JsonNode =
+  %("0x" & toHex(v))
 
-proc `%`*(v: Address): JsonNode =
-  result = %("0x" & array[20, byte](v).toHex)
+func `%`*(v: Address): JsonNode =
+  %("0x" & array[20, byte](v).toHex)
 
-proc `%`*(v: TypedTransaction): JsonNode =
-  result = %("0x" & distinctBase(v).toHex)
+func `%`*(v: TypedTransaction): JsonNode =
+  %("0x" & distinctBase(v).toHex)
 
 proc writeHexValue(w: JsonWriter, v: openArray[byte]) =
   w.stream.write "\"0x"
@@ -127,19 +153,19 @@ proc readValue*(r: var JsonReader, T: type Address): T =
 proc readValue*(r: var JsonReader, T: type TypedTransaction): T =
   T fromHex(seq[byte], r.readValue(string))
 
-proc `$`*[N](v: FixedBytes[N]): string {.inline.} =
+func `$`*[N](v: FixedBytes[N]): string {.inline.} =
   "0x" & array[N, byte](v).toHex
 
-proc `$`*(v: Address): string {.inline.} =
+func `$`*(v: Address): string {.inline.} =
   "0x" & array[20, byte](v).toHex
 
-proc `$`*(v: TypedTransaction): string {.inline.} =
+func `$`*(v: TypedTransaction): string {.inline.} =
   "0x" & distinctBase(v).toHex
 
-proc `$`*(v: DynamicBytes): string {.inline.} =
+func `$`*(v: DynamicBytes): string {.inline.} =
   "0x" & toHex(v)
 
-proc `%`*(x: EthSend): JsonNode =
+func `%`*(x: EthSend): JsonNode =
   result = newJObject()
   result["from"] = %x.source
   if x.to.isSome:
@@ -155,7 +181,7 @@ proc `%`*(x: EthSend): JsonNode =
   if x.nonce.isSome:
     result["nonce"] = %x.nonce.unsafeGet
 
-proc `%`*(x: EthCall): JsonNode =
+func `%`*(x: EthCall): JsonNode =
   result = newJObject()
   result["to"] = %x.to
   if x.source.isSome:
@@ -169,10 +195,10 @@ proc `%`*(x: EthCall): JsonNode =
   if x.data.isSome:
     result["data"] = %x.data.unsafeGet
 
-proc `%`*(x: byte): JsonNode =
+func `%`*(x: byte): JsonNode =
   %x.int
 
-proc `%`*(x: FilterOptions): JsonNode =
+func `%`*(x: FilterOptions): JsonNode =
   result = newJObject()
   if x.fromBlock.isSome:
     result["fromBlock"] = %x.fromBlock.unsafeGet
@@ -183,8 +209,7 @@ proc `%`*(x: FilterOptions): JsonNode =
   if x.topics.isSome:
     result["topics"] = %x.topics.unsafeGet
 
-proc `%`*(x: RtBlockIdentifier): JsonNode =
+func `%`*(x: RtBlockIdentifier): JsonNode =
   case x.kind
   of bidNumber: %(&"0x{x.number:X}")
   of bidAlias: %x.alias
-
