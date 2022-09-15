eeb38f09c88c34b5658c3f1cf08eb86e4150ce69
Will Whitty <tavurth@gmail.com>
2022-09-15 13:41:57 +0400

    Fix problem with contract out of assets

---
+++ simulator.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Watch all nim files for changes
+# When a file change is detected we will restart ganache-cli
+# This ensures that our deposit contracts have enough ETH as
+# it seems like some of the tests do not properly initialise
+# their contracts at this time. (state persists across runs)
+
+nodemon --ext '.nim' --watch tests --watch web3 --exec "ganache-cli"
