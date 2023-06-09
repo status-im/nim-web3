1f9fa11d0e63c16aa4ec30e1f7328ae61254d7d0
tersec <tersec@users.noreply.github.com>
2023-06-09 20:06:35 +0000

    https://github.com/ethereum/execution-apis/pull/407 (#87)

--- web3/engine_api_callsigs.nim
+++ web3/engine_api_callsigs.nim
@@ -5,7 +5,7 @@ import ethtypes, engine_api_types
 
 proc engine_newPayloadV1(payload: ExecutionPayloadV1): PayloadStatusV1
 proc engine_newPayloadV2(payload: ExecutionPayloadV2): PayloadStatusV1
-proc engine_newPayloadV3(payload: ExecutionPayloadV3): PayloadStatusV1
+proc engine_newPayloadV3(payload: ExecutionPayloadV3, versioned_hashes: seq[VersionedHash]): PayloadStatusV1
 proc engine_forkchoiceUpdatedV1(forkchoiceState: ForkchoiceStateV1, payloadAttributes: Option[PayloadAttributesV1]): ForkchoiceUpdatedResponse
 proc engine_forkchoiceUpdatedV2(forkchoiceState: ForkchoiceStateV1, payloadAttributes: Option[PayloadAttributesV2]): ForkchoiceUpdatedResponse
 proc engine_getPayloadV1(payloadId: PayloadID): ExecutionPayloadV1
