faf3ac609f7352b41c6b462b27741e461bcbe298
Agnish Ghosh <iamagnix@gmail.com>
2025-04-01 17:31:51 +0530

    Revert "prepare for getBlobsV2"
    This reverts commit 5ec4dac0cd423d88fbce12917f6917fb3ea5bdfb.

--- web3/execution_types.nim
+++ web3/execution_types.nim
@@ -56,8 +56,6 @@ type
     shouldOverrideBuilder*: Opt[bool]
     executionRequests*: Opt[seq[seq[byte]]]
 
-  GetBlobsResponse* = seq[BlobAndProofV1]
-
   Version* {.pure.} = enum
     V1
     V2
@@ -89,9 +87,6 @@ func version*(res: GetPayloadResponse): Version =
     Version.V2
   else:
     Version.V1
-
-func version*(res: GetBlobsResponse): Version =
-  Version.V1
 
 func V1V2*(attr: PayloadAttributes): PayloadAttributesV1OrV2 =
   PayloadAttributesV1OrV2(
@@ -393,9 +388,6 @@ func V4*(res: GetPayloadResponse): GetPayloadV4Response =
     executionRequests: res.executionRequests.get,
   )
 
-func V1*(res: GetBlobsResponse): GetBlobsV1Response =
-  res
-
 func getPayloadResponse*(x: ExecutionPayloadV1): GetPayloadResponse =
   GetPayloadResponse(executionPayload: x.executionPayload)
 
@@ -421,6 +413,3 @@ func getPayloadResponse*(x: GetPayloadV4Response): GetPayloadResponse =
     shouldOverrideBuilder: Opt.some(x.shouldOverrideBuilder),
     executionRequests: Opt.some(x.executionRequests),
   )
-
-func getBlobsResponse*(x: GetBlobsV1Response): GetBlobsResponse =
-  x
