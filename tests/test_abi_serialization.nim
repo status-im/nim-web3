0d7d73b5df6bfefa77a490a4f0d432792a5d1873
Arnaud <arnaud@status.im>
2025-07-30 17:29:35 +0200

    Add support for dontSerialize pragma

--- tests/test_abi_serialization.nim
+++ tests/test_abi_serialization.nim
@@ -1,21 +1,25 @@
 import
     std/unittest,
     serialization,
+    ../web3/encoding,
     ../web3/decoding,
     ../web3/eth_api_types,
     ../web3/abi_serialization,
     ./helpers/primitives_utils
 
+
+type Contract = object
+  a: uint64
+  b {.dontSerialize.}: string
+  c: bool
+
 suite "ABI serialization":
   test "encode and decode tuple":
-    let fromAddr = address(3)
-    let toAddr = address(4)
-    let amount: uint64 = 42
+    let x = Contract(a: 1, b: "SECRET", c: true)
 
-    let payload = ( fromAddr,  toAddr,  amount)
+    let encoded = Abi.encode(x)
+    let decoded = Abi.decode(encoded, Contract)
 
-    let encoded = Abi.encode(payload)
-    check encoded.len > 0
-
-    let decoded = Abi.decode(encoded, typeof(payload))
-    check decoded == payload
+    check decoded.a == x.a
+    check decoded.b == ""
+    check decoded.c == x.c
