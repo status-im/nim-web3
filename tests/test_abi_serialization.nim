5c430057f7c1a623ab2b11d5811bd420f90eefb8
Arnaud <arnaud@status.im>
2025-08-06 20:29:41 +0200

    Ensure that object are encoded as tuple

--- tests/test_abi_serialization.nim
+++ tests/test_abi_serialization.nim
@@ -7,19 +7,21 @@ import
     ../web3/abi_serialization,
     ./helpers/primitives_utils
 
-
 type Contract = object
   a: uint64
   b {.dontSerialize.}: string
   c: bool
+  d: string
 
 suite "ABI serialization":
-  test "encode and decode tuple":
-    let x = Contract(a: 1, b: "SECRET", c: true)
+  test "encode and decode custom type":
+    let x = Contract(a: 1, b: "SECRET", c: true, d: "hello")
 
     let encoded = Abi.encode(x)
-    let decoded = Abi.decode(encoded, Contract)
+    check encoded == Abi.encode((x.a, x.c, x.d))
 
+    let decoded = Abi.decode(encoded, Contract)
     check decoded.a == x.a
     check decoded.b == ""
     check decoded.c == x.c
+    check decoded.d == x.d
