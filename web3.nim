32b8999d03399a9a970cd24e06954508da0aaae8
Constantine Molchanov <moigagoo@duck.com>
2026-08-06 16:22:58 +0400

    Remove discard from future.
    Discarding futures is discouraged:
    https://status-im.github.io/nim-chronos/tips.html#discard

--- web3.nim
+++ web3.nim
@@ -230,7 +230,7 @@ proc subscribeForLogs*(w: Web3, options: FilterOptions,
                       {.async: (raises: [CatchableError]).} =
   result = await subscribe(w, "logs", Opt.some(options), logsHandler, errorHandler)
   if withHistoricEvents:
-    discard getHistoricalEvents(result, options)
+    await getHistoricalEvents(result, options)
   else:
     result.historicalEventsProcessed = true
 
