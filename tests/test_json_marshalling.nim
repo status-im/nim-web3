1f8da6255120f4e4ac157a076165fde6d40f189c
tersec <tersec@users.noreply.github.com>
2026-08-13 10:23:47 +0000

    foo

--- tests/test_json_marshalling.nim
+++ tests/test_json_marshalling.nim
@@ -126,6 +126,7 @@ template checkRandomObject(T: type) =
 
 suite "JSON-RPC Quantity":
   test "Random object encoding":
+    checkRandomObject(SyncObject)
     checkRandomObject(Withdrawal)
     checkRandomObject(AccessPair)
     checkRandomObject(AccessListResult)
